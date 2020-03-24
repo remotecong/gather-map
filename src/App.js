d43b72f79ff05c484d7735b25219a55ca644e78e
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-03-23 22:28:34 -0500

    going back in time was all I needed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map, LayersControl, Marker, Popup } from 'react-leaflet';
+import { GoogleMutant, GoogleApiLoader } from 'react-leaflet-googlemutant';
 import 'leaflet/dist/leaflet.css';
 
 function App() {
   const [zoom, setZoom] = React.useState(13);
   const [position, setPosition] = React.useState([51.505, -0.09]);
+  const { BaseLayer } = LayersControl;
 
   return (
-    <Map center={position} zoom={zoom} style={{height: '100vh'}}>
-      <TileLayer
-        attribution={'&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS4 popup.<br />Easily customizable.
-        </Popup>
-      </Marker>
-    </Map>
+    <>
+      <GoogleApiLoader apiKey="AIzaSyCaMRWqlcMu96wUqotTCAwxNXPIH8Rif6c">
+        <Map center={position} zoom={zoom} style={{height: '100vh'}}>
+          <LayersControl position='topright'>
+            <BaseLayer checked name='Google Maps Roads'>
+              <GoogleMutant type="roadmap"/>
+            </BaseLayer>
+            <BaseLayer name='Google Maps Satellite'>
+              <GoogleMutant type="satellite" />
+            </BaseLayer>
+          </LayersControl>
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS4 popup.<br />Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
+      </GoogleApiLoader>
+    </>
   );
 }
 
